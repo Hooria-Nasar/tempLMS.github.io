d438817227cc46e621f59bbe11ebe0d8ee41add2
fahadkhanchughtai <fahadkhanchughtai@gmail.com>
2020-05-16 16:44:19 +0500

    google recaptcha added

--- email.php
+++ email.php
@@ -1,5 +1,24 @@
 <?php
 
+// A function definition for google recaptcha
+function reCaptcha($recaptcha)
+{
+    $secret = "6LfhKPgUAAAAAHpbhonXfSK7QFWtZfrO-zX4PQ8t";
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $postvars = array("secret" => $secret, "response" => $recaptcha, "remoteip" => $ip);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($data, true);
+}s
+
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\Exception;
@@ -17,8 +36,8 @@ $dotenv->load();
 
 // Replace sender@example.com with your "From" address.
 // This address must be verified with Amazon SES.
-$sender = 'contact@learninghub.pk';
-$senderName = $_POST['name'];;
+$sender = 'fahadkhanchughtai@gmail.com';
+$senderName = $_POST['name'];
 
 // Replace recipient@example.com with a "To" address. If your account
 // is still in the sandbox, this address must be verified.
@@ -43,7 +62,7 @@ $port =  getenv('SMTP_PORT');
 // The subject line of the email
 $subject = 'Contact';
 
- 
+
 // contact form data
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -54,10 +73,18 @@ $message = $_POST['message'];
 $bodyText =  $message;
 
 // The HTML-formatted body of the email
-$bodyHtml = 'Sender:    '. $name .'<br> Sender Email:   '. $email . '<br> Sender Phone Number:  ' . $phone . '<br> containing following message:<br>'.$message ;
+$bodyHtml = 'Sender:    ' . $name . '<br> Sender Email:   ' . $email . '<br> Sender Phone Number:  ' . $phone . '<br> containing following message:<br>' . $message;
 
 $mail = new PHPMailer(true);
 
+
+
+// the code for captcha
+$recaptcha = $_POST['g-recaptcha-response'];
+$res = reCaptcha($recaptcha);
+if (!$res['success']) {
+    // Error
+}
 try {
     // Specify the SMTP settings.
     $mail->isSMTP();
@@ -80,7 +107,7 @@ try {
     $mail->Body       = $bodyHtml;
     $mail->AltBody    = $bodyText;
     $mail->Send();
-    echo "Email sent!" , PHP_EOL;
+    echo "Email sent!", PHP_EOL;
 } catch (phpmailerException $e) {
     echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
 } catch (Exception $e) {
